6004216c4f34d01acdbccba37e8d6aa8b02e48cc
Clinto Abraham <clinto92@gmail.com>
2023-02-14 19:31:01 +0530

    migrated to newest react 18 version

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from './Utils/exports'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { SnackbarProvider } from 'notistack';
+import { store } from './redux/store';
+import './Styles/index.css'
+import App from './App';
+import ErrorPage from './Pages/NotFound';
+import PrivacyPolicy from './Pages/Privacy-Policy';
+import TermsConditions from './Pages/Terms&Conditions';
+import About from './Pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/privacy-&-policy",
+    element: <PrivacyPolicy />,
+  },
+  {
+    path: "/terms-of-service",
+    element: <TermsConditions />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <SnackbarProvider maxSnack={3}>
+        <RouterProvider router={router} />
+      </SnackbarProvider>
+    </Provider >
+  </React.StrictMode >,
+)
