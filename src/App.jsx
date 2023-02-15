62a202a8ac328153ddd55ad9ea686ffe2bb225b4
Clinto Abraham <clinto92@gmail.com>
2023-02-15 15:17:45 +0530

    removed react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,67 @@
-import { useState } from './Utils/exports'
+import { Typography, useState } from './Utils/exports'
 import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query'
 
-import TopContainer from './Components/TopContainer'
-import { MiddleContainer } from './Components/MidContainer'
-import { Contact } from './Components/Contact'
-import { CreateSkillCards, MySkills } from './Components/Skills'
-import WordOfGod from './Components/QuotesWordOfGod'
-import FooterNavbar from './Components/Footer'
+import TopContainer from './components/TopContainer'
+import { MiddleContainer } from './components/MidContainer'
+import { Contact } from './components/Contact'
+import { CreateSkillCards, MySkills } from './components/Skills'
+import WordOfGod from './components/QuotesWordOfGod'
+import FooterNavbar from './components/Footer'
 import './Styles/App.css'
 
-function App() {
-  const [queryClient] = useState(() => new QueryClient())
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TopContainer />
-      <MiddleContainer />
-      {/* <NavigateMe /> */}
-      {/* <WebsiteTile /> */}
-      <MySkills CreateSkillCards={CreateSkillCards} />
-      <Contact />
-      <WordOfGod color='white' size='2rem 1rem' />
-      <FooterNavbar />
-    </QueryClientProvider>
-  )
-}
-
-export default App
-
 // import { useState } from 'react'
-// // import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 // import './App.css'
 
+
 // function App() {
+//   const [queryClient] = useState(() => new QueryClient())
 
 //   return (
-//     <div className="App">
-
-//     </div>
+//     <QueryClientProvider client={queryClient}>
+//       <TopContainer />
+//       <MiddleContainer />
+//       {/* <NavigateMe /> */}
+//       {/* <WebsiteTile /> */}
+//       <MySkills CreateSkillCards={CreateSkillCards} />
+//       <Contact />
+//       <WordOfGod color='white' size='2rem 1rem' />
+//       <FooterNavbar />
+//     </QueryClientProvider>
 //   )
 // }
 
 // export default App
+
+
+
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <TopContainer />
+      <MiddleContainer />
+      <MySkills CreateSkillCards={CreateSkillCards} />
+      <Contact />
+      <WordOfGod color='black' size='2rem 1rem' />
+      {/* <TopContainer />
+      <MiddleContainer /> */}
+      {/* <NavigateMe /> */}
+      {/* <WebsiteTile /> */}
+      {/* <MySkills CreateSkillCards={CreateSkillCards} /> */}
+      {/* <Contact /> */}
+      {/* <WordOfGod color='white' size='2rem 1rem' /> */}
+      {/* <FooterNavbar /> */}
+    </div>
+  )
+}
+
+export default App
 
