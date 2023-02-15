f55e98091c4e3f111dc3caad985661181a628ad5
Clinto Abraham <clinto92@gmail.com>
2023-02-15 14:47:35 +0530

    reverting react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,48 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from './Utils/exports'
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+
+import TopContainer from './Components/TopContainer'
+import { MiddleContainer } from './Components/MidContainer'
+import { Contact } from './Components/Contact'
+import { CreateSkillCards, MySkills } from './Components/Skills'
+import WordOfGod from './Components/QuotesWordOfGod'
+import FooterNavbar from './Components/Footer'
+import './Styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <div className="App">
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <TopContainer />
+      <MiddleContainer />
+      {/* <NavigateMe /> */}
+      {/* <WebsiteTile /> */}
+      <MySkills CreateSkillCards={CreateSkillCards} />
+      <Contact />
+      <WordOfGod color='white' size='2rem 1rem' />
+      <FooterNavbar />
+    </QueryClientProvider>
   )
 }
 
 export default App
 
-
-// import { useState } from './Utils/exports'
-// import {
-//   QueryClient,
-//   QueryClientProvider
-// } from '@tanstack/react-query'
-// import './Styles/App.css'
-// import TopContainer from './Components/TopContainer'
-// import { MiddleContainer } from './Components/MidContainer'
-// import { Contact } from './Components/Contact'
-// import { CreateSkillCards, MySkills } from './Components/Skills'
-// import WordOfGod from './Components/QuotesWordOfGod'
-// import FooterNavbar from './Components/Footer'
+// import { useState } from 'react'
+// // import reactLogo from './assets/react.svg'
+// import './App.css'
 
 // function App() {
-//   const [queryClient] = useState(() => new QueryClient())
 
 //   return (
-//     <QueryClientProvider client={queryClient}>
-//       <TopContainer />
-//       <MiddleContainer />
-//       {/* <NavigateMe /> */}
-//       {/* <WebsiteTile /> */}
-//       <MySkills CreateSkillCards={CreateSkillCards} />
-//       <Contact />
-//       <WordOfGod color='white' size='2rem 1rem' />
-//       <FooterNavbar />
-//     </QueryClientProvider>
+//     <div className="App">
+
+//     </div>
 //   )
 // }
 
 // export default App
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,55 +1,57 @@
-import React from 'react'
+
+
+
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from './Utils/exports'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Provider } from 'react-redux';
+import { SnackbarProvider } from 'notistack';
+import { store } from './redux/store';
+import './Styles/index.css'
+import App from './App';
+import ErrorPage from './Pages/NotFound';
+import PrivacyPolicy from './Pages/Privacy-Policy';
+import TermsConditions from './Pages/Terms&Conditions';
+import About from './Pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/privacy-&-policy",
+    element: <PrivacyPolicy />,
+  },
+  {
+    path: "/terms-of-service",
+    element: <TermsConditions />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <SnackbarProvider maxSnack={3}>
+        <RouterProvider router={router} />
+      </SnackbarProvider>
+    </Provider >
+  </React.StrictMode >,
 )
 
-
-// import React from 'react';
-// import { RouterProvider, createBrowserRouter } from './Utils/exports'
+// import React from 'react'
 // import ReactDOM from 'react-dom/client'
-// import { Provider } from 'react-redux';
-// import { SnackbarProvider } from 'notistack';
-// import { store } from './redux/store';
-// import './Styles/index.css'
-// import App from './App';
-// import ErrorPage from './Pages/NotFound';
-// import PrivacyPolicy from './Pages/Privacy-Policy';
-// import TermsConditions from './Pages/Terms&Conditions';
-// import About from './Pages/About';
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/privacy-&-policy",
-//     element: <PrivacyPolicy />,
-//   },
-//   {
-//     path: "/terms-of-service",
-//     element: <TermsConditions />,
-//   },
-//   {
-//     path: "/about",
-//     element: <About />,
-//   },
-// ]);
-
+// import App from './App'
+// import './index.css'
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
-//     <Provider store={store}>
-//       <SnackbarProvider maxSnack={3}>
-//         <RouterProvider router={router} />
-//       </SnackbarProvider>
-//     </Provider >
-//   </React.StrictMode >,
+//     <App />
+//   </React.StrictMode>,
 // )
