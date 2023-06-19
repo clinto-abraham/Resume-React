ce215f4f3f6698878ba810dfe32d3900450cc766
Clinto Abraham <clinto92@gmail.com>
2023-06-19 17:20:17 +0530

    deploy sass loader fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import sass from "sass";
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 7000,
   },
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       includePaths: ["node_modules"],
-  //     },
-  //   },
-  // },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
 });
+
+// css: {
+//   preprocessorOptions: {
+//     scss: {
+//       includePaths: ["node_modules"],
+//     },
+//   },
+// },
