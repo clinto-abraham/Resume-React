1aed18af5dda4293ad876205266af3fad31065d0
Clinto Abraham <clinto92@gmail.com>
2023-06-19 21:25:32 +0530

    re-amped without swc

--- README.md
+++
@@ -1,3 +0,0 @@
-# Resume-React
-Clinto Resume of github with active link : https://clinto92.github.io/Resume/index.html has been upgraded to react app with active reference : https://clinto.netlify.app/, which in turn will help for expanding resume page with different re-usable cards and anchor the various deployed websites within one website as portfolio.
-## Find latest netlify deploy of this react app on : https://clinto.netlify.app/
--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 7000,
-  },
-});
-
-// import { resolve } from "path";
-// import sass from "sass";
-
-// resolve: {
-//   alias: {
-//     $fonts: resolve("./public"),
-//   },
-// },
-// css: {
-//   preprocessorOptions: {
-//     scss: {
-//       implementation: sass,
-//     },
-//   },
-// },
-
-// css: {
-//   preprocessorOptions: {
-//     scss: {
-//       includePaths: ["node_modules"],
-//     },
-//   },
-// },
-
-// const config = {
-//   resolve: {
-//     alias: {
-//       $fonts: resolve('./public')
-//     }
-//   }
-// };
+})
